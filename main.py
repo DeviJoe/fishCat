fbc7ae2d8d40f1d1cc1a65ec392f4fa149e079b4
Khakimov Lev <hakimov_l_n@sc.vsu.ru>
2021-08-06 12:31:26 +0300

    Add whois

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     url1 = "http://letoctf.org"
     sideCheck = SideServiceChecker.SideServiceChecker(url)
     sideCheck1 = SideServiceChecker.SideServiceChecker(url1)
-    asyncio.get_event_loop().run_until_complete(sideCheck.is_google_secure_check_fishing())
-    asyncio.get_event_loop().run_until_complete(sideCheck1.is_google_secure_check_fishing())
+    # asyncio.get_event_loop().run_until_complete(sideCheck.is_google_secure_check_fishing())
+    # asyncio.get_event_loop().run_until_complete(sideCheck1.is_google_secure_check_fishing())
+    asyncio.get_event_loop().run_until_complete(sideCheck1.whois_info_json())
+
     pass
